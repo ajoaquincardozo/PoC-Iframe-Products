fea00374db12c482f27318f8e00a7cdd71931eb3
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-27 13:44:05 -0300

    Se agrega log para ver origen recibio

--- src/Components/Routes/RoutingListener.tsx
+++ src/Components/Routes/RoutingListener.tsx
@@ -4,6 +4,8 @@ import { Navigation, AllowedDomains } from ".";
 export const RoutingListener = () => {
   useEffect(() => {
     window.addEventListener('message', function (event) {
+      console.log("Origin recibido", event.origin);
+
       if (AllowedDomains.includes(event.origin)){
         const data: Navigation = JSON.parse(event.data);
         document.cookie = `${data.cookie.name}=${data.cookie.value}; SameSite=None; Secure`;
